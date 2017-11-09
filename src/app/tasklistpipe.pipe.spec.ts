b67025d8c5619320f054b84e7174181189680dba
erald <e.guyader@itexo.fr>
2017-11-09 16:13:50 +0100

    2017_11_9

---
+++ src/app/tasklistpipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TasklistpipePipe } from './tasklistpipe.pipe';
+
+describe('TasklistpipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new TasklistpipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
